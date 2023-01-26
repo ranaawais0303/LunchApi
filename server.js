6eb1763b74f990b25eee4d37711db2bb287f27c7
Rana_awais <z.manzoor@pioneerlogics.com>
2023-01-26 02:30:30 -0800

    create environment variable and connect code with db

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+const mongoose = require("mongoose");
+const dotenv = require(`dotenv`);
+
 const app = require(`./app`);
-const port = 3000;
+
+/////set environment variables here
+dotenv.config({ path: "config.env" });
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+//connection with database
+
+mongoose.connect(DB).catch((error) => console.error(error));
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`app running on port ${port}...`);
+  console.log("db connection successful");
 });
