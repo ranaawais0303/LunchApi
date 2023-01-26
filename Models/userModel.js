2fc740d683ba97158d68447360d9c5c9b0c255f3
Rana_awais <z.manzoor@pioneerlogics.com>
2023-01-26 01:35:50 -0800

    apply cors and create user schema

---
+++ Models/userModel.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name"],
+    // maxlength: [40, "A User's Name must have less or equal then 40 char"],
+    // minlength: [6, "A User's Name must have more or equal then 6 char"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowerCase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  photo: String,
+
+  role: {
+    type: String,
+    enum: ["user", "guide", "lead-guide", "admin"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (el) {
+        //this only works on CREATE and SAVE not on update etc
+        return el === this.password;
+      },
+      message: "Confirm Password ({VALUE}) should be  Equal to  password",
+    },
+  },
+  passwordChangedAt: Date,
+
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+});
--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 
+///Server startup
 const app = express();
 app.get("/", (req, res) => {
   res.status(200).send("Hellow from the server side");
 });
+
+///for all type of frontend
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+//
 module.exports = app;
